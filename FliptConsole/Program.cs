79eb2ad16d0208ceeb7c19ac11ad966b8e4bcc35
Michael West <michaellwest@gmail.com>
2020-08-08 18:39:22 -0500

    Added demo code.

---
+++ FliptConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+using Flipt;
+using Grpc.Core;
+
+namespace FliptConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var channel = new Channel("127.0.0.1:9000", ChannelCredentials.Insecure);
+            var client = new Flipt.Flipt.FliptClient(channel);
+            var entityId = Guid.NewGuid().ToString();
+
+            var response = client.Evaluate(new EvaluationRequest {EntityId = entityId, FlagKey = "new-login"});
+            Console.WriteLine(JsonSerializer.Serialize(response));
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
